2153aeca9d14c1f31dcf81543fb1f4a37e6d1ad1
iStoik <vuoksa@bk.ru>
2022-07-15 23:37:05 +0300

    Job2

---
+++ Task_13AlternativeSolution/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: "); // Ввод значения пользователем
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int thirdNumber = ThirdNumber(number);
+Console.WriteLine(thirdNumber);
+
+
+static int ThirdNumber(int count)
+{
+    while (count <= -1000 || count >= 1000) count /= 10;
+    int digit = count % 10;
+    return digit;
+}
